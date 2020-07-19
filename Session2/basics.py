8ae00b6eae42c47266d6bfe0e030d4989cf712cb
Utkarsh Sharma <flamingice2801@gmail.com>
2020-07-19 21:31:07 +0530

    Create basics.py

---
+++ Session2/basics.py
@@ -0,0 +1,19 @@
+'''Today's class will cover the basics of
+Python programming language'''
+
+my_float = 5.5553322132434
+
+# This is a single line comment
+
+''' This 
+is
+a multi line 
+comment'''
+
+print ("Hello World!")
+
+x = 5555
+y = "Utkarsh"
+print(x)
+print(y)
+'''
